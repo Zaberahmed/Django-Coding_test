d21c744f2e68429f4ad4181f4cf8a36a40363671
zaber ahmed <zaber0305@gmail.com>
2022-07-25 04:08:31 +0600

    Made Migrations

--- django-coding-test/src/product/admin.py
+++ django-coding-test/src/product/admin.py
@@ -1,3 +1,11 @@
+from itertools import product
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Variant)
+admin.site.register(Product)
+admin.site.register(ProductVariant)
+admin.site.register(ProductImage)
+admin.site.register(ProductVariantPrice)
